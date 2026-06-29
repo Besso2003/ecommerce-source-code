5303aa79ec094a3517cd8fab40aa5983fe12bf0a
Besso2003 <Bassant.ali23@icloud.com>
2026-06-29 17:42:51 +0300

    ci: add manifest update step for multi-service builds

--- src/cart/src/Program.cs
+++ src/cart/src/Program.cs
@@ -19,7 +19,7 @@ using OpenFeature;
 using OpenFeature.Contrib.Providers.Flagd;
 using OpenFeature.Contrib.Hooks.Otel;
 
-// triggring action1
+// triggring action
 var builder = WebApplication.CreateBuilder(args);
 string valkeyAddress = builder.Configuration["VALKEY_ADDR"];
 if (string.IsNullOrEmpty(valkeyAddress))
